bbe768bb7ebaf1b13b98e4dbe8c1b2917f273de1
tlc3004 <leonantoniobm@gmail.com>
2025-08-05 20:38:06 -0500

    adjust

--- src/custom/useSound.js
+++ src/custom/useSound.js
@@ -2,7 +2,7 @@
 export const useSound = (src) => {
   const play = () => {
     const audio = new Audio(src);
-    audio.volume = 0.5;
+    audio.volume = 0.4;
     audio.play();
   };
   return play;
