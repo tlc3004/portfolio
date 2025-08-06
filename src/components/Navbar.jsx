c5d8d8d16acaf91fd746a55506c99f43c3031844
tlc3004 <leonantoniobm@gmail.com>
2025-08-05 20:17:07 -0500

    Summary

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,8 @@ export default function Navbar({ onSelect }) {
     "PERFIL",
     "EDUCACION",
     "EXPERIENCIA",
-    "PROYECTOS_DESTACADOS",
-    "LOGROS_PERSONALES",
-    "MI_SUSHI"
+    "PROYECTOS",
+    "EXPERIENCIA_PERSONAL"
   ];
 
   return (
